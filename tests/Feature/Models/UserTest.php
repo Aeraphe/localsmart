65638988c72b7e20ec9cbc53d057cc556823a47a
aeraphe <alberto.aeraph@gmail.com>
2021-07-08 23:02:23 -0300

    test: assert in delete user model

--- tests/Feature/Models/UserTest.php
+++ tests/Feature/Models/UserTest.php
@@ -21,13 +21,29 @@ class UserTest extends TestCase
     {
         //arrange
         $userName = "Maria Dalva de Castro";
-        
+
         //act
         $user = User::factory()->create(['name' => $userName]);
 
         //assert
         $this->assertInstanceOf(User::class, $user, "create a user model");
         $this->assertDatabaseHas('users', ['name' => $userName]);
+
+    }
+
+    /**
+     * @test
+     */
+    public function delete_user()
+    {
+
+        //arrange
+        $userName = 'Alberto Eduardo de Oliveira' . rand();
+        $user = User::factory()->create(['name' => $userName]);
+        //act
+        $user->delete();
+        //assert
+        $this->assertDatabaseMissing('users', ['name' => $userName]);
 
     }
 
