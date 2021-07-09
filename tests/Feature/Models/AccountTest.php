8c814dc6761665b6ea3d3967de6c63fbb6c2544f
aeraphe <alberto.aeraph@gmail.com>
2021-07-09 11:28:48 -0300

    test: account creation test

---
+++ tests/Feature/Models/AccountTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AccountTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * Create Account
+     *@test
+     * @return void
+     */
+    public function create_user_app_account()
+    {
+        //arrange
+        $user = User::factory()->create();
+        $data = [
+            'plan_name' => 'free',
+            'plan_status' => true,
+            'store_qt' => 1,
+            'user_id' => $user->id,
+        ];
+
+        //act
+        $account = Account::create($data);
+
+        //assert
+        $this->assertInstanceOf(Account::class, $account);
+        $this->assertDatabaseHas('accounts',$data);
+    }
+}
