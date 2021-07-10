30126b5d52d36f6a9d32603e35ef15e813fc4774
aeraphe <alberto.aeraph@gmail.com>
2021-07-10 16:50:38 -0300

    feat(factory): create a factory for Customers

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Account;
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Customer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        return [
+            'name' => $this->faker->name,
+            'address' => $this->faker->address,
+            'cpf' => $this->faker->randomLetter,
+            'account_id' => Account::factory(),
+            'phone' => $this->faker->phoneNumber,
+            'city' => $this->faker->city,
+            'state' => $this->faker->country,
+            'obs' => $this->faker->text,
+        ];
+    }
+}
