266dee0109c902f552cc0aa2f970852e4c7caa06
aeraphe <alberto.aeraph@gmail.com>
2021-07-10 17:36:47 -0300

    test: make a test for check if equipament can be created

---
+++ tests/Feature/Models/EquipamentTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Equipament;
+use Tests\TestCase;
+
+class EquipamentTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_create_equipoament()
+    {
+
+        //arrange and act
+        $result = Equipament::factory()->create();
+
+        //assert
+        $this->assertInstanceOf(Equipament::class, $result);
+        $this->assertDatabaseHas('equipaments', ['id' => $result->id]);
+
+    }
+}
