9d40f787f209bc79ccdcc31f36fed5ed0fd8442f
aeraphe <alberto.aeraph@gmail.com>
2021-07-11 22:09:38 -0300

    feat(routes): create routes for admin login and staff login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/admin/login',[LoginController::class,'authenticateAdmin']);
+
+Route::post('/login/{account?}/{store?}',[LoginController::class,'authenticateStaff']);
