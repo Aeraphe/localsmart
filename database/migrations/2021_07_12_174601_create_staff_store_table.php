4e8769c06332168c8be3f99b4465acbe31b11890
aeraphe <alberto.aeraph@gmail.com>
2021-07-12 14:51:07 -0300

    feat(database): create a migration for relationship many to many staff and store

---
+++ database/migrations/2021_07_12_174601_create_staff_store_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff_store', function (Blueprint $table) {
+            $table->foreignId('staff_id')->constrained();
+            $table->foreignId('store_id')->constrained();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff_store');
+    }
+}
