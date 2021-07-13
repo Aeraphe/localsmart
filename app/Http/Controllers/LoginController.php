8d0b2224bcd21aeb7369ce81a2c751eb9c7e1e12
aeraphe <alberto.aeraph@gmail.com>
2021-07-12 21:04:50 -0300

    feat(controllers): create login method for staff users

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
+use App\Models\Staff;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -32,11 +34,49 @@ class LoginController extends Controller
             }
 
             return back()->withErrors([
-                'email' => 'The provided credentials do not match our records.',
+                'error' => 'The provided credentials do not match our records.',
             ]);
 
         } catch (\Exception$e) {
-            return response()->json(['message' => 'user not found!'], 404);
+            return response()->json(['error' => 'user not found!'], 404);
+        }
+
+    }
+
+    public function authenticateEmploye(Request $request)
+    {
+        try {
+            $credentials = $request->validate([
+                'login_name' => ['required'],
+                'password' => ['required'],
+            ]);
+
+            $accountSlug = $request->route('account');
+            $storeSlug = $request->route('store');
+
+            $account = Account::where('slug', $accountSlug)->firstOrFail();
+
+            $employeQuery = [['login_name', '=', $credentials['login_name']], ['account_id', '=', $account->id]];
+            $employe = Staff::where($employeQuery)->firstOrFail();
+
+            $stores = $employe->stores;
+
+            $store = $stores->firstWhere('slug', $storeSlug);
+
+            if (Hash::check($credentials['password'], $employe->password) && $store->status) {
+                $request->session()->regenerate();
+                return response()->json(['data' => [
+                    'name' => $employe->name,
+                    'store' => $store->name,
+                ]], 200);
+            }
+
+            return back()->withErrors([
+                'error' => 'The provided credentials do not match our records.',
+            ]);
+
+        } catch (\Exception$e) {
+            return response()->json(['error' => 'user not found!'], 404);
         }
 
     }
