2df10409b6552f55f5dec7060d821098e6871ea6
aeraphe <alberto.aeraph@gmail.com>
2021-07-13 20:26:40 -0300

    feat(modules): delete equipament module scaffold

--- Modules/Equipament/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('equipament::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('equipament.name') !!}
-    </p>
-@endsection
