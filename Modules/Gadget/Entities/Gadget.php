cdbd312c519f2dfd95e6a5e9ef24bca759d7d3c8
aeraphe <alberto.aeraph@gmail.com>
2021-07-14 16:47:22 -0300

    feat(gadget-models): create gadget model

--- Modules/Gadget/Entities/Gadget.php
+++ Modules/Gadget/Entities/Gadget.php
@@ -2,17 +2,21 @@
 
 namespace Modules\Gadget\Entities;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Gadget extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
-    
+    protected $fillable = [
+        'name',
+        'gadget_type_id',
+        'manufacturer_id',
+    ];
+
     protected static function newFactory()
     {
-        return \Modules\Gadget\Database\factories\GadgetFactory::new();
+        return \Modules\Gadget\Database\factories\GadgetFactory::new ();
     }
 }
