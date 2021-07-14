f479545e512f32b9d496d4ff40db75191dd272b3
aeraphe <alberto.aeraph@gmail.com>
2021-07-14 11:18:28 -0300

    test(gadget): make test for check should create manufacturer

---
+++ Modules/Gadget/Tests/Unit/Entities/ManufacturerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Gadget\Tests\Unit\Entities;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Modules\Gadget\Entities\Manufacturer as EntitiesManufacturer;
+use Tests\TestCase;
+
+class Manufacturer extends TestCase
+{
+
+    use WithFaker, RefreshDatabase;
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function can_create_gadget_manufactorer()
+    {
+        //arrange
+        $data = ['name' => 'Apple'];
+
+        //act
+        $result = EntitiesManufacturer::factory()->create($data);
+
+        //assert
+        $this->assertInstanceOf(EntitiesManufacturer::class, $result);
+        $this->assertDatabaseHas('manufacturers', $data);
+
+    }
+}
