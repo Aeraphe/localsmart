06d5d7623993213bbc4976b45a6ee7f71ba69c41
aeraphe <alberto.aeraph@gmail.com>
2021-07-15 16:08:27 -0300

    feat(database): create migration for InvoiceEquipamentCondition model

---
+++ database/migrations/2021_07_15_185031_create_invoice_equipament_conditions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceEquipamentConditionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_equipament_conditions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('repair_invoice_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_equipament_conditions');
+    }
+}
