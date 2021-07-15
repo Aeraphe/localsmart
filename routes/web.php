bee53bc384a6eda4402e842a957ce867182795a8
aeraphe <alberto.aeraph@gmail.com>
2021-07-15 12:55:43 -0300

    feat(routes): delete web login routes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::post('/admin/login',[LoginController::class,'authenticateAdmin']);
-
-Route::post('/login/{account}/{store}',[LoginController::class,'authenticateEmploye']);
