163750cf21454dace8fc4dd8a295443c1a90b2ae
aeraphe <alberto.aeraph@gmail.com>
2021-07-16 05:19:19 -0300

    feat(routes): create web route for account login with session

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('account')->group(function () {
+    Route::post('/login', [LoginController::class, 'authenticateAccountUserWeb'])->name('auth-admin');
+});
+
