7578004b558a548763804b69945fb54a9a5dfd03
aeraphe <alberto.aeraph@gmail.com>
2021-07-22 20:36:32 -0300

    feat(routes): create accout register api route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\UserAccountRegister;
+use App\Http\Controllers\AccountRegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,7 +24,7 @@ Route::prefix('v1')->group(function () {
 
     Route::prefix('account')->group(function () {
         Route::post('/login', [LoginController::class, 'authenticateAccountUserApi'])->name('api-auth-admin');
-        Route::post('/register', [UserAccountRegister::class, 'create'])->name('api-register-account');
+        Route::post('/register', [AccountRegisterController::class, 'create'])->name('api-register-account');
     });
 
     Route::post('/login/{account}/{store}', [LoginController::class, 'authenticateEmployeApi'])->name('api-auth-employe');
