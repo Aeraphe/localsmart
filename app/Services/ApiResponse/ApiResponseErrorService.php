b35b34bf9804ba85f1c73920b998530688052692
aeraphe <alberto.aeraph@gmail.com>
2021-07-22 15:04:10 -0300

    feat(services): update for ApiResponseErrorService for receive custom exception and php default exception

--- app/Services/ApiResponse/ApiResponseErrorService.php
+++ app/Services/ApiResponse/ApiResponseErrorService.php
@@ -2,23 +2,33 @@
 
 namespace App\Services\ApiResponse;
 
+
 use Illuminate\Http\Response;
 
 class ApiResponseErrorService
 {
 
-    public static function make(\Exception $ex, array $error = null)
+    public static function make($exception)
     {
 
-       
+        if (!method_exists($exception, 'getData')) {
+            $error = ['error' => $exception->getMessage()];
+            $message = 'Erro inesperado. Favor entrar em contato com o responsável pelo aplicativo';
+            $status = 500;
+        } else {
+            $error = ['error' => $exception->getData()];
+            $message = $exception->getMessage();
+            $status = $exception->getCode();
+        }
+
         $responseData = [
             'error' => $error,
-            '_message' => $ex->getMessage(),
-            '_status' => $ex->getCode(),
+            '_message' => $message,
+            '_status' => $status,
             '_url' => request()->url(),
             '_method' => request()->method(),
         ];
 
-        return new Response($responseData,$ex->getCode());
+        return new Response($responseData, $exception->getCode());
     }
 }
