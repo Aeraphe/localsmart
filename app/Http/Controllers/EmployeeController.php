cba8f0e83d3f29b20e0129ed9b92e84137ed7284
aeraphe <alberto.aeraph@gmail.com>
2021-07-23 13:34:34 -0300

    feat(controllers): create employee controller with create method

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EmployeeRegisterRequest;
+use App\Services\ApiResponse\ApiResponseErrorService;
+use App\Services\ApiResponse\ApiResponseService;
+use App\Services\RegisterEmployeeService;
+use Exception;
+
+class EmployeeController extends Controller
+{
+
+    /**
+     * Register new employee in the User Account
+     *
+     * @param EmployeeRegisterRequest $request
+     * 
+     * @return ApiResponseService | ApiResponseErrorService
+     */
+    public function create(EmployeeRegisterRequest $request)
+    {
+
+        try {
+
+            $data = $request->validated();
+
+            $employee = RegisterEmployeeService::create($data);
+
+            $responseData = ['name' => $employee->name, 'login' => $employee->login_name];
+
+            return ApiResponseService::make('Funcionário criado com sucesso', 200, $responseData);
+
+        } catch (Exception $e) {
+
+            return ApiResponseErrorService::make($e);
+        }
+    }
+}
