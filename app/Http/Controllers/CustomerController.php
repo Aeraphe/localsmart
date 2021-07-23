09b5ade249253b427f842dde1e5b7c2bec8a47c1
aeraphe <alberto.aeraph@gmail.com>
2021-07-23 16:31:05 -0300

    feat(controllers): make controller and method for create customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterCustomerRequest;
+use App\Services\ApiResponse\ApiResponseErrorService;
+use App\Services\ApiResponse\ApiResponseService;
+use App\Services\RegisterCustomerService;
+use Exception;
 
 class CustomerController extends Controller
 {
-    //
+    /**
+     * Create customer
+     *
+     * @param RegisterCustomerRequest $request
+     * @return void
+     */
+    public function create(RegisterCustomerRequest $request)
+    {
+        try {
+            $data = $request->validated();
+
+            $customer = RegisterCustomerService::create($data);
+            
+            $response = ['name' => $customer->name];
+            
+            return ApiResponseService::make('Cliente Cadastrado com sucesso', 200, $response);
+
+        } catch (Exception $e) {
+
+            return ApiResponseErrorService::make($e);
+        }
+    }
 }
