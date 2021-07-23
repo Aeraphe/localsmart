8d2f38033782d679ccfa87239a8057c9bb8605ca
aeraphe <alberto.aeraph@gmail.com>
2021-07-22 22:29:17 -0300

    feat(routes): create post customer route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\AccountRegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +26,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('account')->group(function () {
         Route::post('/login', [LoginController::class, 'authenticateAccountUserApi'])->name('api-auth-admin');
         Route::post('/register', [AccountRegisterController::class, 'create'])->name('api-register-account');
+        Route::middleware('auth:api')->post('/customer', [CustomerController::class, 'create'])->name('create-customer');
     });
 
     Route::post('/login/{account}/{store}', [LoginController::class, 'authenticateEmployeApi'])->name('api-auth-employe');
