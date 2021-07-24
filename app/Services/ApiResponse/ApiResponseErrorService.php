6b32d99c958859ca0dddcbd10e47798ed37977c2
aeraphe <alberto.aeraph@gmail.com>
2021-07-24 11:03:07 -0300

    feat(services): create exception error messages  handle

--- app/Services/ApiResponse/ApiResponseErrorService.php
+++ app/Services/ApiResponse/ApiResponseErrorService.php
@@ -7,27 +7,81 @@ use Illuminate\Http\Response;
 class ApiResponseErrorService
 {
 
+    /**
+     * Create Reponse Exception
+     *
+     * @param [Exception] $exception
+     *
+     * @return Illuminate\Http\Response
+     */
     public static function make($exception)
     {
 
-        if (!method_exists($exception, 'getData')) {
-            $error =  ['internal' => $exception->getMessage()];
-            $message = 'Erro inesperado. Favor entrar em contato com o responsável pelo aplicativo';
-            $status = 500;
-        } else {
-            $error = $exception->getData();
-            $message = $exception->getMessage();
-            $status = $exception->getCode() == 0 ? 500 : $exception->getCode();
-        }
+        $status = $exception->getCode() == 0 ? 500 : $exception->getCode();
 
         $responseData = [
-            'errors' => $error,
-            '_message' => $message,
+            'errors' => self::exceptionErrorHandle($exception),
+            '_message' => self::exceptionMessageHandle($exception),
             '_status' => $status,
             '_url' => request()->url(),
             '_method' => request()->method(),
         ];
 
-        return new Response($responseData, 200);
+        return new Response($responseData, $status);
     }
+
+    /**
+     * Handle for get Exception Errors
+     *
+     * @param [Exception] $exception
+     * @return array | string
+     */
+    private static function exceptionErrorHandle($exception)
+    {
+
+        if (!method_exists($exception, 'getData')) {
+
+            return ['internal' => $exception->getMessage()];
+
+        } else {
+
+            return $exception->getData();
+
+        }
+
+    }
+
+    /**
+     * Handle Exception Messages
+     *
+     * @param [Exception] $exception
+     * @return [string] $message
+     */
+    private static function exceptionMessageHandle($exception)
+    {
+
+        switch ($exception::class) {
+
+            case 'App\Exceptions\BaseException':
+
+                $message = $exception->getMessage();
+                break;
+
+            case 'App\Exceptions\RegisterEmployeeLoginException':
+
+                $message = "Nome para login já cadastrado";
+                break;
+
+            case 'Illuminate\Auth\Access\AuthorizationException':
+
+                $message = 'Operação não autorizada, entre em contato com o administrador do sistema';
+                break;
+
+            default:
+                $message = $exception->getMessage();
+                break;
+        }
+        return $message;
+    }
+
 }
