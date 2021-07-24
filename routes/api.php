df520cc4ee17927acc2a381130515bce8a925000
aeraphe <alberto.aeraph@gmail.com>
2021-07-24 15:53:59 -0300

    feat(routes): create new route for customer equipament

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AccountRegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\EquipamentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,7 +30,9 @@ Route::prefix('v1')->group(function () {
         Route::post('/register', [AccountRegisterController::class, 'create'])->name('api-register-account');
         Route::middleware('auth:api')->post('/customer', [CustomerController::class, 'create'])->name('create-customer');
         Route::middleware('auth:api')->post('/employee', [EmployeeController::class, 'create'])->name('create-employee');
+        Route::middleware('auth:api')->post('/customer/equipament', [EquipamentController::class, 'create'])->name('create-customer-equipament');
     });
+
 
     Route::post('/login/{account}/{store}', [LoginController::class, 'authenticateEmployeApi'])->name('api-auth-employe');
 });
