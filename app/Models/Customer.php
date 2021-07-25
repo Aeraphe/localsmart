d33ba230e73bc6d26e89c2ccb78ba0b889e65bba
aeraphe <alberto.aeraph@gmail.com>
2021-07-25 11:31:54 -0300

    feat(models): add soft delete on customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     * soft deleted
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 
     /**
      * Attributes that are mass assignable
