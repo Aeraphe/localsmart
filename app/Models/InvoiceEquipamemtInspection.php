c42782a1aa450c9d6ce057380eaa3ae5029de53d
aeraphe <alberto.aeraph@gmail.com>
2021-07-25 11:03:06 -0300

    feat(models): create InvoiceEquipamemtInspection model

---
+++ app/Models/InvoiceEquipamemtInspection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceEquipamemtInspection extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['repair_invoice_id', 'name'];
+}
