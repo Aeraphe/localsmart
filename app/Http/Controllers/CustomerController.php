e1093b74d247eff3cef5149f33c5373322848c61
aeraphe <alberto.aeraph@gmail.com>
2021-07-25 17:35:06 -0300

    feat(controllers): create delete customer method on CustomerControler

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DeleteCustomerRequest;
 use App\Http\Requests\RegisterCustomerRequest;
+use App\Models\Customer;
 use App\Services\ApiResponse\ApiResponseErrorService;
 use App\Services\ApiResponse\ApiResponseService;
 use App\Services\RegisterCustomerService;
@@ -22,13 +24,36 @@ class CustomerController extends Controller
             $data = $request->validated();
 
             $customer = RegisterCustomerService::create($data);
-            
+
             $response = ['name' => $customer->name];
-            
+
             return ApiResponseService::make('Cliente Cadastrado com sucesso', 200, $response);
 
         } catch (Exception $e) {
 
+            return ApiResponseErrorService::make($e);
+        }
+    }
+
+    /**
+     * Delete Customer
+     *
+     * @param DeleteCustomerRequest $request
+     * 
+     * @return ApiResponseService
+     */
+    public function delete(DeleteCustomerRequest $request)
+    {
+
+        try {
+
+            $this->authorize('delete_customer');
+            $validated = $request->validated();
+            Customer::where('id', '=', $validated['id'])->delete();
+
+            return ApiResponseService::make('Cliente apagado com sucesso', 202, ['deleted' => $validated['id']]);
+
+        } catch (Exception $e) {
             return ApiResponseErrorService::make($e);
         }
     }
