45ccb2ebbc6778ca479b15e15617115aea2649f8
aeraphe <alberto.aeraph@gmail.com>
2021-07-26 16:58:16 -0300

    feat(requests): create a request for edit customer equipament

---
+++ app/Http/Requests/EditCustomerEquipamentRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Equipament;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class EditCustomerEquipamentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $equipamentId = Request::get('id');
+        $accountId = Auth::user()->account->id;
+        $equipament = Equipament::find($equipamentId);
+
+
+        if ($accountId === $equipament->customer->account_id) {
+
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => ['required', 'numeric'],
+            'name' => ['nullable'],
+            'gadget_id' => ['nullable', 'numeric'],
+        ];
+    }
+}
