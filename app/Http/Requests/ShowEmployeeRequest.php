d85aaf3bb9434f2f9bc53e22c59501431b21d740
aeraphe <alberto.aeraph@gmail.com>
2021-07-26 10:36:43 -0300

    feat(request): create request for authorize get employee

---
+++ app/Http/Requests/ShowEmployeeRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ShowEmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = Auth::user();
+        $employee = Request::route('employee');
+        $role = $user->roles->where('name', 'admin')->first()->name;
+
+        //If user is admin and employe and user are from same account
+        if ($role === 'admin' && $employee->account->id == $user->account->id) {
+            return true;
+            //If employee and user are the same person
+        } elseif ($employee->id == $user->id) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
