a3fe96bc995c078d2ed7ae66faa10f1bd7dedb4c
aeraphe <alberto.aeraph@gmail.com>
2021-07-26 07:14:28 -0300

    feat(controllers): create method showAll to get all customers on CustomerController

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,10 +6,12 @@ use App\Http\Requests\DeleteCustomerRequest;
 use App\Http\Requests\RegisterCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
+use App\Models\User;
 use App\Services\ApiResponse\ApiResponseErrorService;
 use App\Services\ApiResponse\ApiResponseService;
 use App\Services\RegisterCustomerService;
 use Exception;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerController extends Controller
 {
@@ -85,9 +87,29 @@ class CustomerController extends Controller
     {
 
         try {
-
+            $this->authorize('show_customer');
             $responseData = $customer->toArray();
             return ApiResponseService::make('Consulta realizada com sucesso', 200, $responseData);
+        } catch (Exception $e) {
+            return ApiResponseErrorService::make($e);
+        }
+    }
+
+    /**
+     * List all customers
+     *
+     * @return void
+     */
+    public function showAll()
+    {
+        try {
+            $this->authorize('show_customer');
+            $accountId = Auth::user()->account->id;
+            $customers =  Customer::where('account_id',$accountId)->get();
+
+
+            return ApiResponseService::make('Consulta Realizada com Sucesso!!!',200,$customers->toArray());
+
         } catch (Exception $e) {
             return ApiResponseErrorService::make($e);
         }
