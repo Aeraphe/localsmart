c87f9bc75aea48066acc40407f3c30fc9cab9329
aeraphe <alberto.aeraph@gmail.com>
2021-07-26 09:00:08 -0300

    feat(requests): create request for delete Employee

---
+++ app/Http/Requests/DeleteEmployeeRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Employee;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DeleteEmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $employeId = Request::get('id');
+        $user = Auth::user();
+        $employe = Employee::where('account_id', $user->account->id)
+            ->where('id', $employeId)
+            ->first();
+
+        if ($employe) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => ['required', 'numeric'],
+        ];
+    }
+}
