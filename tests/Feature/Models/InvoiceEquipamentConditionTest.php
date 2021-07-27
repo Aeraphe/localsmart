33f696e0e19cce750daad1b37229e52e39c2e009
aeraphe <alberto.aeraph@gmail.com>
2021-07-27 16:45:11 -0300

    test(models): add prop equipament_id on test can_create_invoice_equipament_condition

--- tests/Feature/Models/InvoiceEquipamentConditionTest.php
+++ tests/Feature/Models/InvoiceEquipamentConditionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Equipament;
 use App\Models\InvoiceEquipamentCondition;
 use App\Models\RepairInvoice;
 use Tests\TestCase;
@@ -10,14 +11,19 @@ class InvoiceEquipamentConditionTest extends TestCase
 {
     /**
      * @test
-     *
+     * group equipament
      * @return void
      */
     public function can_create_invoice_equipament_condition()
     {
         //arrange
-        $invoice = RepairInvoice::factory()->create();
-        $data = ['name' => 'Without test condition', 'repair_invoice_id' => $invoice->id];
+
+        $invoice = RepairInvoice::factory()->has(Equipament::factory()->count(1))->create();
+        $data = [
+            'name' => 'Without test condition',
+            'repair_invoice_id' => $invoice->id,
+            'equipament_id' => $invoice->equipament->id,
+        ];
 
         //act
         $result = InvoiceEquipamentCondition::factory()->create($data);
