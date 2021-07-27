2dc8f8208355ad978180acddf0105aff3c2b88d1
aeraphe <alberto.aeraph@gmail.com>
2021-07-27 17:37:54 -0300

    fet(requests): create a request form for use in show RepairInvoices

---
+++ app/Http/Requests/ShowRepairInvoiceRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\RepairInvoice;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ShowRepairInvoiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $invoiceId = Request::route('id');
+        $user = Auth::user();
+        $invoice = RepairInvoice::find($invoiceId);
+        $store = $user->stores->find($invoice->store_id);
+        if ($store) {
+            Request::route()->setParameter('invoice', $invoice);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
