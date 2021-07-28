5a8c3ad613afb77827a95ecf5f02b287f7225183
aeraphe <alberto.aeraph@gmail.com>
2021-07-28 19:03:53 -0300

    feat(controllers): create a controller method for update equipament inspection

--- app/Http/Controllers/Invoice/EquipamentInspectionController.php
+++ app/Http/Controllers/Invoice/EquipamentInspectionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Invoice;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Invoice\CreateEquipamentInspectionRequest;
 use App\Http\Requests\Invoice\DeleteEquipamentInspectionRequest;
+use App\Http\Requests\Invoice\UpdateEquipamentInspectionRequest;
 use App\Models\InvoiceEquipamemtInspection;
 use App\Models\RepairInvoice;
 use App\Services\ApiResponse\ApiResponseErrorService;
@@ -55,6 +56,30 @@ class EquipamentInspectionController extends Controller
             InvoiceEquipamemtInspection::find($validated['id'])->delete();
 
             return ApiResponseService::make('Inspeção Apagada com sucesso!!!', 200, $validated);
+
+        } catch (Exception $th) {
+
+            return ApiResponseErrorService::make($th);
+
+        }
+    }
+
+    /**
+     * Update equipament Inspection
+     *
+     * @param UpdateEquipamentInspectionRequest $request
+     * @return void
+     */
+    public function update(UpdateEquipamentInspectionRequest $request)
+    {
+        try {
+
+            $this->authorize('update_equipament_inspection');
+            $validated = $request->validated();
+
+            InvoiceEquipamemtInspection::find($validated['id'])->update($validated);
+
+            return ApiResponseService::make('Inspeção Atualizada com sucesso!!!', 200, $validated);
 
         } catch (Exception $th) {
 
