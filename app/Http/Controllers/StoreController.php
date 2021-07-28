77b6d6ba4565032860c321432b7bfeab7cdf65b6
aeraphe <alberto.aeraph@gmail.com>
2021-07-27 22:35:57 -0300

    feat(controllers): create a method show on StoreController to list all stores

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ApiResponse\ApiResponseErrorService;
+use App\Services\ApiResponse\ApiResponseService;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+
+class StoreController extends Controller
+{
+    public function show()
+    {
+        try {
+            $this->authorize('show_all_store');
+            $user = Auth::user();
+            $storeResponseData = $user->account->stores->toArray();
+            return ApiResponseService::make('Consulta realizada com sucesso', 200, $storeResponseData);
+        } catch (Exception $e) {
+            return ApiResponseErrorService::make($e);
+        }
+    }
+}
