d44a825daeb3026dfc2e083285b03eb3e4e8fffb
aeraphe <alberto.aeraph@gmail.com>
2021-07-28 18:52:21 -0300

    feat(requests): create a form request for authorize to delete equipament inspection

---
+++ app/Http/Requests/Invoice/DeleteEquipamentInspectionRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Invoice;
+
+use App\Models\InvoiceEquipamemtInspection;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class DeleteEquipamentInspectionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = Request::user();
+     
+        $id = Request::get('id');
+        
+        $inspection = InvoiceEquipamemtInspection::find($id);
+    
+        $storeId = $inspection->repairInvoice->store->id;
+
+        if ($user->stores->where('id', $storeId)->first()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => ['required', 'numeric'],
+        ];
+    }
+}
