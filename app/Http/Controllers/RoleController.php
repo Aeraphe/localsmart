ae86ee4d2fa6276b94308419fb71758ebf3dfe23
aeraphe <alberto.aeraph@gmail.com>
2021-08-12 12:47:16 -0300

    feat(controllers): create sign method on RoleControler

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Role\SignRequest;
+use App\Models\Employee;
 use App\Services\ApiResponse\ApiResponseErrorService;
 use App\Services\ApiResponse\ApiResponseService;
 use Exception;
@@ -10,6 +12,13 @@ use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
+    /**
+     * Show all user Roles and App default Roles
+     * App default roles account_id is null
+     *
+     * @param Request $request
+     * @return ApiResponseService<Role>
+     */
     public function show(Request $request)
     {
         try {
@@ -23,6 +32,29 @@ class RoleController extends Controller
             return ApiResponseService::make('Consulta Realizada com sucesso!!!', 200, $roles->toArray());
         } catch (Exception $e) {
             return ApiResponseErrorService::make($e);
+        }
+    }
+
+    /**
+     * Sign Role to Employee
+     *
+     * @return void
+     */
+    public function sign(SignRequest $request)
+    {
+        try {
+            $this->authorize('sign_role');
+            $validated = $request->validated();
+            $employee = Employee::find($validated['employee_id']);
+            $role = Role::findById($validated['role_id']);
+            $employee->assignRole($role->name);
+
+            return ApiResponseService::make('Permissão atribuida com Sucesso!!!',200,$validated);
+
+        } catch (Exception $e) {
+
+            return ApiResponseErrorService::make($e);
+
         }
     }
 }
