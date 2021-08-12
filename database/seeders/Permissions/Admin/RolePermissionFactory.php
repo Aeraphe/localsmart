4c1ca5d4afd76c31aa337c7dd51bfc1b58bf985c
aeraphe <alberto.aeraph@gmail.com>
2021-08-12 11:27:04 -0300

    feat(seeders): create a RolePermissionFactory

---
+++ database/seeders/Permissions/Admin/RolePermissionFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders\Permissions\Admin;
+
+use Database\Seeders\Permissions\PermissionFactory;
+
+class RolePermissionFactory extends PermissionFactory{
+
+
+    public static $module = 'admin';
+
+    public static $scope = 'role';
+
+    public static $permissions = [
+
+        [
+            "name" => 'create',
+            "description" => 'Adicionar um novo tipo de Usuário',
+            'roles' => ['admin'],
+        ],
+        [
+            "name" => 'delete',
+            "description" => 'Apagar tipo de Usuário',
+            'roles' => ['admin'],
+        ],
+        [
+            "name" => 'show',
+            "description" => 'Ver tipo de Usuário',
+            'roles' => ['admin'],
+        ],
+        [
+            "name" => 'show_all',
+            "description" => 'Listar todos os tipos de Usuários',
+            'roles' => ['admin'],
+        ],
+        [
+            "name" => 'update',
+            "description" => 'Atualizar o tipo de Usuário',
+            'roles' => ['admin'],
+        ],
+
+    ];
+
+
+
+
+}
