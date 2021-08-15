086c91176701f32b0884c157b3e618169c1e4984
aeraphe <alberto.aeraph@gmail.com>
2021-08-15 14:35:55 -0300

    feat(routes): create a route for detach employee from store

--- routes/api.php
+++ routes/api.php
@@ -99,7 +99,8 @@ Route::prefix('v1')->group(function () {
 
             //Store
             Route::post('/', [StoreController::class, 'create'])->name('create-store');
-            Route::post('/sign/employee', [StoreController::class, 'sign'])->name('create-store');
+            Route::post('/sign/employee', [StoreController::class, 'sign']);
+            Route::delete('/unsign/employee', [StoreController::class, 'unsign']);
             Route::delete('/', [StoreController::class, 'delete'])->name('delete-store');
             Route::put('/', [StoreController::class, 'update'])->name('update-store');
             Route::get('/', [StoreController::class, 'showAll'])->name('show-all-store');
